eb41e9a403551e97f4f5470c63d471017234d3e4
Borislava Kornovska <105539662+bokornovska@users.noreply.github.com>
2023-04-20 09:28:14 +0300

    Express.js - templating

--- 03. ExpressJS and Templating/demo/index.js
+++ 03. ExpressJS and Templating/demo/index.js
@@ -1,6 +1,10 @@
 const express = require('express'); //imoprt express
+const handlebars = require('express-handlebars');
 
 const server = express(); //create server
+
+server.engine('handlebars', handlebars.engine());
+server.set('view engine', 'handlebars')
 
 const loggerMiddleware = require('./loggerMiddleware');
 
@@ -8,6 +12,10 @@ server.use(express.static('public'));
 server.use(loggerMiddleware);
 
 server.get('/', (req, res) => {
+    res.render('home')
+});
+
+server.get('/old', (req, res) => {
     res.send(`
     <html>
         <head>
@@ -25,7 +33,14 @@ server.get('/', (req, res) => {
 });
 
 server.get('/cats', (req, res) => {
-    res.send('<h1>CATS Page</H1>');
+
+    const cats = [
+        { name: 'Navcho', breed: 'Persian', age: 7 },
+        { name: 'Sisa', breed: 'Angora', age: 12 },
+        { name: 'Zuza', breed: 'Street cat', age: 10 },
+    ]
+
+    res.render('cats', { cats });
 });
 
 server.get('/cats/1', (req, res) => {
@@ -43,24 +58,26 @@ server.get('/cats/3', (req, res) => {
 let validateCatIdMiddleware = (req, res, next) => {
     let catId = Number(req.params.catId);
 
-    if(!catId){
+    if (!catId) {
         return res.send('<h1>Invalid catId</h1>')
         // return res.redirect('/404')
     }
-    
+
+    req.catId = catId;
     next();
 };
 
 
 server.get('/cats/:catId', validateCatIdMiddleware, (req, res) => {
-    res.send(`<h1>Individual Cat Page - catId = ${req.params.catId}</H1>`)
+    res.render('cat', { id: req.params.catId, isOdd: req.catId %2 !==0})
 });
 
 server.get('/dogs', (req, res) => {
-    res.send('<h1>DOGS Page</H1>');
+    res.render('dogs');
 });
 
 server.post('/cats', (req, res) => {
+
     res.send('cat recieved')
 });
 
