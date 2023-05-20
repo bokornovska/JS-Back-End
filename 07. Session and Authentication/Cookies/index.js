77daecc7bf2f1af4f21aa5d9578b278d11745578
Borislava Kornovska <105539662+bokornovska@users.noreply.github.com>
2023-05-20 23:12:31 +0300

    Demo-Cookies

---
+++ 07. Session and Authentication/Cookies/index.js
@@ -0,0 +1,66 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+
+const app = express();
+
+app.use(express.urlencoded({extended: false}));
+app.use(cookieParser());
+
+app.get('/', (req,res) => {
+    res.send(`
+    <h1>Hello from Homepage</h1>
+    <p><a href="/login">Login</a></p>
+    <p><a href="/profile">Profile</a></p>
+
+    `);
+})
+
+app.get('/login', (req, res) => {
+    res.send(`<form method ="POST">
+    <label for="username">Username</label>
+    <input type="text" id="username" name="username" />
+
+    <label for="password"></label>
+    <input type="password" id="password" name="password" />
+
+    <input type="submit" value="login" />
+
+</form>`);
+});
+
+app.post('/login', (req, res) => {
+    const {username, password} = req.body;
+
+    if(username == 'Ivan' && password == 'V'){
+        const data = {
+            username : "Ivan",
+            age : 25
+        }
+
+        res.cookie('auth', JSON.stringify(data));
+        return res.redirect('/')
+    }
+
+    res.status(401).end();
+});
+
+app.get('/profile', (req,res) => {
+    // check if user is logged?
+
+    const authData = req.cookies['auth'];
+
+    if(!authData){
+        return res.status(401).end();
+    }
+
+    const {username} = JSON.parse(authData);
+
+    console.log(username);
+
+    res.send(`
+    <h2>Hello ${username}</h2>
+    `)
+
+});
+
+app.listen(5000, () => console.log('Server is listening on port 5000...'));
