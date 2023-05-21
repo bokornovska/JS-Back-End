cfd9057eb261fd8036929dde90a0b398cf855173
Borislava Kornovska <105539662+bokornovska@users.noreply.github.com>
2023-05-21 13:26:15 +0300

    demo-session

--- 07. Session and Authentication/Cookies/index.js
+++ 07. Session and Authentication/Cookies/index.js
@@ -1,12 +1,19 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
+const expressSession = require('express-session');
 
 const app = express();
 
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(expressSession({
+    secret: 'my secret',
+    resave: false,
+    saveUnitialized: true,
+    cookie: { secure: false }
+}))
 
-app.get('/', (req,res) => {
+app.get('/', (req, res) => {
     res.send(`
     <h1>Hello from Homepage</h1>
     <p><a href="/login">Login</a></p>
@@ -29,33 +36,36 @@ app.get('/login', (req, res) => {
 });
 
 app.post('/login', (req, res) => {
-    const {username, password} = req.body;
+    const { username, password } = req.body;
 
-    if(username == 'Ivan' && password == 'V'){
+    if (username == 'Ivan' && password == 'V') {
         const data = {
-            username : "Ivan",
-            age : 25
+            username: "Ivan",
+            age: 25
         }
 
         res.cookie('auth', JSON.stringify(data));
+        req.session.username = 'Ivan';
+        req.session.privateInfo = 'Some private info';
         return res.redirect('/')
     }
 
     res.status(401).end();
 });
 
-app.get('/profile', (req,res) => {
+app.get('/profile', (req, res) => {
     // check if user is logged?
 
     const authData = req.cookies['auth'];
 
-    if(!authData){
+    if (!authData) {
         return res.status(401).end();
     }
 
-    const {username} = JSON.parse(authData);
+    const { username } = JSON.parse(authData);
 
-    console.log(username);
+    // console.log(username);
+    console.log(req.session);
 
     res.send(`
     <h2>Hello ${username}</h2>
