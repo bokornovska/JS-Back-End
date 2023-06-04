af26c786f503bf9126313371dc8007c740c29147
Borislava Kornovska <105539662+bokornovska@users.noreply.github.com>
2023-06-04 11:03:01 +0300

    details page

--- 10. Exam preparation/03. Crypto Trade/controllers/cryptoController.js
+++ 10. Exam preparation/03. Crypto Trade/controllers/cryptoController.js
@@ -3,33 +3,42 @@ const router = require('express').Router();
 const { isAuth } = require('../middlewares/authMiddleware');
 const cryptoService = require('../services/cryptoService');
 
-const {getErrorMessage} = require('../utils/errorUtils');
+const { getErrorMessage } = require('../utils/errorUtils');
 
 // ----------------------CATALOG-------------------------------------------
 
-router.get('/catalog', async (req,res) => {
+router.get('/catalog', async (req, res) => {
 
     const crypto = await cryptoService.getAll();
 
-    res.render('crypto/catalog', {crypto});
+    res.render('crypto/catalog', { crypto });
 })
 
+// -----------------------------DETAILS-----------------------------------
 
+router.get('/:cryptoId/details', async (req, res) => {
+
+    const crypto = await cryptoService.getOne(req.params.cryptoId);
+
+    const isOwner = crypto.owner == req.user?._id;
+
+    res.render('crypto/details', { crypto, isOwner });
+})
 
 // ------------------------CREATE-------------------------------------------
-router.get('/create', isAuth, (req,res) => {
+router.get('/create', isAuth, (req, res) => {
     res.render('crypto/create');
 });
 
-router.post('/create', isAuth, async (req,res) => {
+router.post('/create', isAuth, async (req, res) => {
 
     const cryptoData = req.body;
 
     try {
         await cryptoService.create(req.user._id, cryptoData);
-        
+
     } catch (error) {
-        return res.status(400).render('crypto/create', {error: getErrorMessage(error)});
+        return res.status(400).render('crypto/create', { error: getErrorMessage(error) });
     }
 
     res.redirect('/crypto/catalog');
