c5d07552657c9a404442cc5c32792d392a84ee11
Borislava Kornovska <105539662+bokornovska@users.noreply.github.com>
2023-06-05 15:45:54 +0300

    validation

--- 10. Exam preparation/03. Crypto Trade/controllers/cryptoController.js
+++ 10. Exam preparation/03. Crypto Trade/controllers/cryptoController.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 
 const { isAuth } = require('../middlewares/authMiddleware');
 const cryptoService = require('../services/cryptoService');
-const {paymentMethodsMap} = require('../constants')
+const { paymentMethodsMap } = require('../constants')
 
 const { getErrorMessage } = require('../utils/errorUtils');
 
@@ -17,12 +17,12 @@ router.get('/catalog', async (req, res) => {
 
 // ------------------------------SEARCH-----------------------------------------
 
-router.get('/search', async (req,res) => {
+router.get('/search', async (req, res) => {
 
-    const {name, paymentMethod} = req.query;
-    const crypto = await cryptoService.search();
-    
-    res.render('crypto/search', {crypto});
+    const { name, paymentMethod } = req.query;
+    const crypto = await cryptoService.search(name, paymentMethod);
+
+    res.render('crypto/search', { crypto });
 });
 
 // -----------------------------DETAILS-----------------------------------
@@ -41,32 +41,38 @@ router.get('/:cryptoId/details', async (req, res) => {
 // --------------------------------BUY--------------------------------------
 
 router.get('/:cryptoId/buy', isAuth, async (req, res) => {
-    await cryptoService.buy(req.user._id, req.params.cryptoId);
 
-    res.redirect(`/crypto/${req.params.cryptoId}/details`);
+    try {
+        await cryptoService.buy(req.user._id, req.params.cryptoId);
+
+        res.redirect(`/crypto/${req.params.cryptoId}/details`);
+    } catch (error) {
+        return res.status(400).render('404', { error: getErrorMessage(error) });
+    }
+
 });
 
 
 // ----------------------------------EDIT---------------------------------------
 
-router.get('/:cryptoId/edit', isAuth, async (req,res) => {
+router.get('/:cryptoId/edit', isAuth, async (req, res) => {
 
     const crypto = await cryptoService.getOne(req.params.cryptoId);
 
     const paymentMethods = Object.keys(paymentMethodsMap).map(key => ({
-        value: key, 
+        value: key,
         label: paymentMethodsMap[key],
         isSelected: crypto.paymentMethod == key,
     }));
-    
 
-    res.render('crypto/edit', {crypto, paymentMethods});
+
+    res.render('crypto/edit', { crypto, paymentMethods });
 });
 
-router.post('/:cryptoId/edit', isAuth, async (req,res) => {
+router.post('/:cryptoId/edit', isAuth, async (req, res) => {
 
     const cryptoData = req.body;
-    
+
     await cryptoService.edit(req.params.cryptoId, cryptoData)
     //todo: check if owner
 
@@ -76,8 +82,8 @@ router.post('/:cryptoId/edit', isAuth, async (req,res) => {
 
 // ----------------------------------DELETE---------------------------------------
 
-router.get('/:cryptoId/delete', isAuth, async (req,res) => {
-    
+router.get('/:cryptoId/delete', isAuth, async (req, res) => {
+
     await cryptoService.delete(req.params.cryptoId)
 
     res.redirect('/crypto/catalog');
--- 10. Exam preparation/03. Crypto Trade/services/cryptoService.js
+++ 10. Exam preparation/03. Crypto Trade/services/cryptoService.js
@@ -5,13 +5,14 @@ exports.getAll = () => Crypto.find({}).lean();
 exports.getOne = (cryptoId) => Crypto.findById(cryptoId).lean();
 
 exports.buy = async (userId, cryptoId) => {
+    
     const crypto = await Crypto.findById(cryptoId);
     // TODO check if user has already bought the crypto
     crypto.buyers.push(userId);
     await crypto.save();
 };
 
-exports.edit = (cryptoId, cryptoData) => Crypto.findByIdAndUpdate(cryptoId, cryptoData);
+exports.edit = (cryptoId, cryptoData) => Crypto.findByIdAndUpdate(cryptoId, cryptoData, {runValidators: true});
 
 exports.create = (ownerId, cryptoData) => Crypto.create({ ...cryptoData, owner: ownerId });
 
@@ -20,15 +21,15 @@ exports.delete = (cryptoId) => Crypto.findByIdAndDelete(cryptoId);
 exports.search = async (name, paymentMethod) => {
 
     let crypto = await this.getAll();
-    
+
     // const {name, paymentMethod} = req.query;
     // const crypto = this.search(name, paymentMethod);
 
-    if(name) {
-        crypto = crypto.filter(x => x.toLowerCase() == name)
+    if (name) {
+        crypto = crypto.filter(x => x.name.toLowerCase() == name)
     }
 
-    if(paymentMethod) {
+    if (paymentMethod) {
         crypto = crypto.filter(x => x.paymentMethod == paymentMethod)
     }
 
