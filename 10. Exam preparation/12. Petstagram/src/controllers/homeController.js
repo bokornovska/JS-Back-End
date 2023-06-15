7327f2c89c85d470c9ba21a950c64b66d3a0e067
Borislava Kornovska <105539662+bokornovska@users.noreply.github.com>
2023-06-15 15:55:34 +0300

    comments

--- 10. Exam preparation/12. Petstagram/src/controllers/homeController.js
+++ 10. Exam preparation/12. Petstagram/src/controllers/homeController.js
@@ -6,6 +6,10 @@ router.get('/', (req,res) => {
 
 router.get('/404', (req,res) => {
     res.render('404');
+});
+
+router.get('/profile', async (req,res) => {
+    res.render('profile')
 })
 
 module.exports = router;
