6da51e4a0c71e568814357207a22ba66cb3aa864
Borislava Kornovska <105539662+bokornovska@users.noreply.github.com>
2023-06-15 13:24:01 +0300

    details page

--- 10. Exam preparation/12. Petstagram/src/controllers/photoController.js
+++ 10. Exam preparation/12. Petstagram/src/controllers/photoController.js
@@ -1,35 +1,46 @@
 const router = require('express').Router();
 
-const { getErrorMessage} = require('../utils/error')
+const { getErrorMessage } = require('../utils/error')
 
 const photoService = require('../services/photoService');
 // -----------------------------------------------CREATE------------------------------------------------------
-router.get('/create', (req,res) => {
+router.get('/create', (req, res) => {
     res.render('photos/create');
 });
 
-router.post('/create', async (req,res) => {
+router.post('/create', async (req, res) => {
     const photoData = req.body;
 
     try {
-        
+
         await photoService.create(req.user._id, photoData);
 
         // chek the right redirect
         res.redirect('/photos/catalog');
 
     } catch (err) {
-        res.render('photos/create', {error: getErrorMessage(err)});
+        res.render('photos/create', { error: getErrorMessage(err) });
     }
 
 });
 
 // ---------------------------------------------------CATALOG----------------------------------------------------------
-router.get('/catalog', async(req,res) => {
+router.get('/catalog', async (req, res) => {
 
     const photos = await photoService.getAll();
-    res.render('photos/catalog', {photos});
+    res.render('photos/catalog', { photos });
 });
+
+// ---------------------------------------------------DETAILS------------------------------------------------------------
+
+router.get('/:photoId/details', async (req, res) => {
+    const photoId = req.params.photoId;
+    const photo = await photoService.getOne(photoId);
+    // if it's populated - photo.owner._id // if it`s not photo.owner;
+    const isOwner = req.user._id == photo.owner._id;
+
+    res.render('photos/details', { photo, isOwner });
+})
 
 
 module.exports = router;
--- 10. Exam preparation/12. Petstagram/src/services/photoService.js
+++ 10. Exam preparation/12. Petstagram/src/services/photoService.js
@@ -4,3 +4,5 @@ exports.create = (ownerId, photoData) => Photo.create({ ...photoData, owner: own
 
 // populate(if want to display owner info)
 exports.getAll = () => Photo.find().lean().populate('owner');
+
+exports.getOne = (photoId) => Photo.findById(photoId).lean().populate('owner');
