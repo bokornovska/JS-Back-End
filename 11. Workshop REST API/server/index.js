cfc6510f1c9356f991014fc9b0946ea585b929ef
Borislava Kornovska <105539662+bokornovska@users.noreply.github.com>
2023-06-20 14:50:29 +0300

    add cros

--- 11. Workshop REST API/server/index.js
+++ 11. Workshop REST API/server/index.js
@@ -1,9 +1,23 @@
 const express = require('express');
+const cors = require('cors');
 
+const routes = require('./routes');
 const app = express();
 
-app.get('/', (req,res) => {
-    res.send('hello world');
-});
+app.use(routes);
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// the first way (other - npm i cors)
+// app.use((req, res, next) => {
+
+//     res.setHeader('Access-Control-Allow-Origin', '*');
+
+//     res.setHeader('Access-Control-Allow-Methods', '*');
+//     res.setHeader('Access-Control-Allow-Headers', '*');
+//     next();
+// });
+app.use(cors());
 
 app.listen(3030, () => console.log('RESTfull server is listening on port 3030...'))
